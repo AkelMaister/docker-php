17650d3af2f6da7decd37d379bab98c9191adfa4
AkelMaister <xakel2009@hotmail.com>
2021-09-13 14:49:41 +0300

    fixes

--- build.sh
+++ build.sh
@@ -2,10 +2,11 @@
 
 set -e
 
-AMQP_VERSION=${AMQP_VERSION:-"$(curl -Is https://pecl.php.net/get/amqp | grep filename | curl -Is https://pecl.php.net/get/amqp | grep filename | sed 's/.*\([[:digit:]]\+\.[[:digit:]]\+\.[[:digit:]]\+\).*/\1/')"}
+AMQP_VERSION=${AMQP_VERSION:-"$(curl -Is https://pecl.php.net/get/amqp | grep filename | sed 's/.*\([[:digit:]]\+\.[[:digit:]]\+\.[[:digit:]]\+.*\.\).*/\1/;s/.$//g')"}
+#curl -Is https://pecl.php.net/get/amqp | grep filename | curl -Is https://pecl.php.net/get/amqp | grep filename | sed 's/.*\([[:digit:]]\+\.[[:digit:]]\+\.[[:digit:]]\+\).*/\1/')"}
 PSR_VERSION=${PSR_VERSION:-"$(git -c 'versionsort.suffix=-' ls-remote --exit-code --refs --sort='version:refname' --tags https://github.com/jbboehr/php-psr.git '*.*.*' | tail --lines=1 | cut --delimiter='/' --fields=3 | sed 's/^v//g')"}
 PHP_VERSIONS=${PHP_VERSIONS:-"7.3,7.4"}
-
+NEWRELIC_VERSION=${NEWRELIC_VERSION:-"5-9.17.1.301"}
 getlatesttag() {
   MAJORMINOR=${1:-"7.4"}
   wget -q https://registry.hub.docker.com/v1/repositories/php/tags -O - | sed -e 's/[][]//g' -e 's/"//g' -e 's/ //g' | tr '}' '\n'  | awk -F: '{print $3}' | grep -e "^${MAJORMINOR}.[0-9]\+$" | sort -n | tail -1
